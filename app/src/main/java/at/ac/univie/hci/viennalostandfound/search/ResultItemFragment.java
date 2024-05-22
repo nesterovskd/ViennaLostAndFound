90717e1b695181bf3af96310fa1e9ef24aeb9420
Isidora Zeljovic <isidorazeljovic1112@gmail.com>
2024-05-22 21:12:27 +0200

    fragment_result_list.xml: button navigation edited

--- app/src/main/java/at/ac/univie/hci/viennalostandfound/search/ResultItemFragment.java
+++ app/src/main/java/at/ac/univie/hci/viennalostandfound/search/ResultItemFragment.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,24 +25,37 @@ public class ResultItemFragment extends Fragment{
     private ResultAdapter adapter;
 
     public ResultItemFragment() {
-        // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_result_list, container, false);
-
-        // Initialize ListView
         listView = view.findViewById(R.id.result_list_view);
-
-        // Set up adapter
-        List<ResultItem> items = createDummyData(); // You need to implement this method
+        List<ResultItem> items = createDummyData();
         adapter = new ResultAdapter(requireContext(), items);
         listView.setAdapter(adapter);
 
+        Button editFiltersButton = view.findViewById(R.id.edit_filters_btn);
+        editFiltersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToSearchFragment();
+            }
+        });
+
         return view;
+    }
+
+    private void navigateToSearchFragment() {
+        SearchFragment searchFragment = new SearchFragment();
+
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment, searchFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     private List<ResultItem> createDummyData() {
